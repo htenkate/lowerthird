f607c7a1440ff05fe4feac5ccf39229b09e99f2f
htenkate <58047686+htenkate@users.noreply.github.com>
2020-04-26 22:08:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -8,11 +8,19 @@ $("#slideshow > div:gt(0)").hide();
 
 setInterval(function() {
   $('#slideshow > div:first')
-    .fadeOut(500)
+    .fadeOut(0)
     .next()
-    .fadeIn(500)
+    .fadeIn(0)
     .end()
-    .appendTo('#slideshow');
-}, 6000);
+    .slideUp("slow")
+     .appendTo('#slideshow');
+}, 3000);
+       
+  ////slides take 3 seconds
     
     });
+ 
+
+$("#slideshow > div").inViewport(function(px){
+    if(px) $(this).addClass("triggeredCSS3") ;
+});
