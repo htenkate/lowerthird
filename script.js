6bb952c663fca7c99346b5b99309e928cf6f3f5c
htenkate <58047686+htenkate@users.noreply.github.com>
2020-04-26 16:09:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+$( document ).ready(function() {
+   
+
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(500)
+    .next()
+    .fadeIn(500)
+    .end()
+    .appendTo('#slideshow');
+}, 6000);
+    
+    });
